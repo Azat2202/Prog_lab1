d5ba1735425de92abd47bf007ad39ab199f529bf
azat222 <azat222@gmail.com>
2022-09-05 13:28:03 +0300

    commit

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
+    public static double dumbFunc(double x){
+        return Math.pow(((Math.pow(Math.pow(x / 0.25, x), 3 - Math.cos(x))) + 4) / (Math.pow((2 / Math.tan(x)), 3)),
+                Math.atan(Math.sin(x)));
+    }
     public static void main(String[] args){
         float[] f = new float[10];
         float n = 19;
@@ -13,16 +14,14 @@ public class Main {
         for(int i = 0; i <= 14; i++){
             x[i] = Math.random() * 26 - 10;
         }
-        List<Integer> nums = new ArrayList<>(List.of(1, 7, 11, 15, 19));
         double[][] t = new double[10][15];
         for(int i = 0; i <= 9; i++){
             for(int j = 0; j <= 14; j++) {
                 if (f[i] == 9) {
                     t[i][j] = Math.tan(0.5 * Math.sin(x[j]));
                 }
-                else if (nums.contains(f[i])){
-                    t[i][j] = Math.pow(((Math.pow(Math.pow(x[j] / 0.25, x[j]), 3 - Math.cos(x[j]))) + 4) / (Math.pow((2 / Math.tan(x[j])), 3)),
-                            Math.atan(Math.sin(x[j])));
+                else if (f[i] == 1 || f[i] == 7 || f[i] == 11 || f[i] == 15 || f[i] == 19){
+                    t[i][j] = Main.dumbFunc(x[j]);
                 }
                 else{
                     t[i][j] = Math.tan(Math.asin(Math.exp(Math.cbrt(Math.abs(x[j]) / -2))));
@@ -33,7 +32,7 @@ public class Main {
             for (int j = 0; j < 14; j++) {
                 System.out.printf(" %.2f ", t[i][j]);
             }
-            System.out.println();
+            System.out.print("\n");
         }
     }
 }
